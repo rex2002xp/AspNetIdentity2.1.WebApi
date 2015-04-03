35ba7c278c1eacc0a6f480f6269da3a391fb7bf9
Victor Cornejo <rex2002xp@gmail.com>
2015-04-03 12:30:56 -0600

    Paso 3. Se crea la clase que servidora con contexto con la base de datos, estamos utilizando "entity framework code-first" . La Clase ApplicationUser nos permite agregar los campos para los nombres, nivel y fecha de enrolamiento que mas adelante nos servirán como datos adicionales del usuario, esta se hereda de IdentityUser.

---
+++ WebApi/Model/Identity/ApplicationUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Model.Identity
+{
+    /// <summary>
+    /// Clase personalizada que hereda de IdentityUser, nos permite agregar los campos de Nombres , Nivel y Fecha de enrolamiento.
+    /// </summary>
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; }
+
+        [Required]
+        public byte Level { get; set; }
+
+        [Required]
+        public DateTime JoinDate { get; set; }
+    }
+}
